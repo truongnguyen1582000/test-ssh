fdff931e939281e2ad290ce43d74b7c1efbbd316
Truong Nguyen <truongnguyen1582000@gmail.com>
2021-05-04 09:36:47 +0700

    change the name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Truong IS very handsome mann !
         </a>
       </header>
     </div>
